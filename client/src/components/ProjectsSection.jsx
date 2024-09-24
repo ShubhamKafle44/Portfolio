5f1747d7173b7b32021488784d8c6e5615e94241
Shubham Kafle <shubhamkafle44@gmail.com>
2024-09-24 11:46:11 -0500

    Updated the projects section with images and fixed the gsap scrolltrigger animation

--- client/src/components/ProjectsSection.jsx
+++ client/src/components/ProjectsSection.jsx
@@ -2,18 +2,37 @@ import React from "react";
 import styled from "styled-components";
 import { forwardRef } from "react";
 import { H1 } from "./styledComponents";
+
+// Styled component for the card
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 20px;
+  border: 1px solid;
+  border-radius: 8px;
+  width: 300px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+// Styled component for text
+const Text = styled.div`
+  margin-top: 10px; /* Add spacing between title and text */
+  text-align: center; /* Center align the text */
+`;
+
 const ProjectsSection = forwardRef(function ProjectsSection(
   { text, title, image, ...props },
   ref
 ) {
   return (
-    <div className="flex justify-center items-center" {...props} ref={ref}>
-      <div>
-        <H1> {title} </H1>
-      </div>
-      {/* <img src={image} alt="Project Image" /> */}
-      <div>{text}</div>
-    </div>
+    <Card {...props} ref={ref}>
+      <H1>{title}</H1>
+      <img src={image} />
+      <Text>{text}</Text>
+    </Card>
   );
 });
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -3,6 +3,9 @@ import { ScrollToPlugin, ScrollTrigger } from "gsap/all";
 import React, { useRef, useEffect } from "react";
 import ProjectsSection from "../components/ProjectsSection";
 import { FixedHeading } from "../components/styledComponents";
+import LANE_DETECTION from "../../public/images/LANE_DETECTION.jpg";
+import CHESS from "../../public/images/CHESS.jpg";
+import IMAGE_CLASSIFICATION from "../../public/images/IMAGE_CLASSIFICATION.jpg";
 
 // Register GSAP plugins
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
@@ -28,7 +31,10 @@ const Projects = () => {
     const t1 = gsap.timeline({
       scrollTrigger: scrollTriggerConfig,
     });
-
+    t1.from(".card1", {
+      xPercent: 75,
+      opacity: 1,
+    });
     t1.addLabel("card1");
     t1.to(".card1", {
       xPercent: 0,
@@ -47,7 +53,7 @@ const Projects = () => {
       {
         scale: 0.95,
         xPercent: -0.5,
-        opacity: 0.5,
+        opacity: 0.15,
       },
       "-=0.3"
     );
@@ -69,11 +75,15 @@ const Projects = () => {
       {
         scale: 0.98,
         xPercent: -0.4,
-        opacity: 0.6,
+        opacity: 0.15,
       },
       "-=0.3"
     );
 
+    t1.to(".card3", {
+      xPercent: 0,
+      opacity: 1,
+    });
     t1.to(".card3", {
       xPercent: 0,
       opacity: 1,
@@ -90,25 +100,25 @@ const Projects = () => {
       <div className="sticky r-0">
         <FixedHeading>Projects</FixedHeading>
       </div>
-      <div className="cards" ref={card}>
+      <div className="cards flex" ref={card}>
         <ProjectsSection
-          title="Project 1"
-          text="Hello1"
-          img="image1"
+          title="Lane Detection"
+          text="Created a lane detection model using Hough Transform and OpenCV for autonomous vehicle tracking"
+          image={LANE_DETECTION}
           className=" card card1"
           ref={card1Ref}
         />
         <ProjectsSection
-          title="Project 2"
-          text="Hello2"
-          img="image1"
+          title="Image Classification Model"
+          text="Developed an image classification model with TensorFlow to optimize performance and accuracy."
+          image={IMAGE_CLASSIFICATION}
           className=" card card2"
           ref={card2Ref}
         />
         <ProjectsSection
-          title="Project 3"
-          text="Hello3"
-          img="image1"
+          title="Multi-player Chess Engine:"
+          text="Built a multi-player chess engine in Python using Minimax and a relational database for efficient data management"
+          image={CHESS}
           className=" card card3"
           ref={card3Ref}
         />
