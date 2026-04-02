c91accfc27242bfc41a707ce62de494034f2af44
shubhamkafle44 <shubhamkafle44@gmail.com>
2026-04-02 12:02:02 -0500

    Update NavBar with HomePage Routing

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom"; // React Router Link
+import { AiFillHome } from "react-icons/ai"; // Home icon
 import Button from "./Button";
 
 const Navigation = () => {
     return (
         <nav className="absolute z-10 p-2 top-0 left-1/2 transform -translate-x-1/2 flex space-x-4 sm:left-auto sm:transform-none sm:top-0">
+
+            {/* Home Icon Button */}
+            <Link to="/">
+                <Button text={<AiFillHome size={20} />} />
+            </Link>
+
+            {/* Other navigation buttons */}
             <Link to="/projects">
                 <Button text="Projects" />
             </Link>
